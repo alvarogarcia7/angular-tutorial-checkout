3c592de5add953c580d87b8e9d45251d1ece43a1
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-07-23 17:52:05 +0200

    Show alerts if price is over 700

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,21 +1,37 @@
-export const products = [
-  {
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
-  },
-  {
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
-  },
-  {
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+export class Product{
+  constructor (obj: any){
+    for(var property in obj){
+      this[property] = obj[property]
+    }
   }
-];
+  isAlert(): boolean{
+    return this.price > 700
+  }
+}
 
+export class Products {
+  values : Array<Product> = [
+    new Product({
+      name: 'Phone XL',
+      price: 799,
+      description: 'A large phone with one of the best screens'
+    }),
+    new Product({
+      name: 'Phone Mini',
+      price: 699,
+      description: 'A great phone with one of the best cameras'
+    }),
+    new Product({
+      name: 'Phone Standard',
+      price: 299,
+      description: ''
+    })
+  ];
+}
+
+let products = new Products();
+
+export {products};
 
 /*
 Copyright Google LLC. All Rights Reserved.
